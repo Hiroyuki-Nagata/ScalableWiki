d39711b33791ad0d9b093842aaa9304261adbfae
hiroyuki nagata <newserver002@gmail.com>
2015-11-05 00:49:29 +0900

    Fix processing by andThen

--- project/build.scala
+++ project/build.scala
@@ -15,6 +15,22 @@ object ScalableWiki extends Build {
     try { op(p) } finally { p.close() }
   }
 
+  val tmplVarToScala = (perlString: String) =>
+  """<!--TMPL_VAR NAME=\"(.*?)\".*-->""".r
+    .replaceAllIn(perlString, m => "@" + m.group(1))
+
+  val tmplIfToScala = (perlString: String) =>
+  """<!--TMPL_IF NAME=\"(.*?)\".*-->(.*?)<!--/TMPL_IF-->""".r
+    .replaceAllIn(perlString, m => "@if (" + m.group(1) + ".nonEmpty) { " + m.group(2) + " } ")
+
+  val tmplUnlessToScala = (perlString: String) =>
+  """<!--TMPL_UNLESS NAME=\"(.*?)\".*-->(.*?)<!--/TMPL_UNLESS-->""".r
+    .replaceAllIn(perlString, m => "@if (" + m.group(1) + ".isEmpty) { " + m.group(2) + " } ")
+
+  val tmplIfElseToScala = tmplIfToScala.andThen(tmplUnlessToScala)
+
+  val tmplVarAndIfElseToScala = tmplVarToScala.andThen(tmplIfElseToScala)
+
   lazy val tmplCommand =
     Command.command("gen-tmpl") { (state: State) =>
 
@@ -32,22 +48,22 @@ object ScalableWiki extends Build {
         fo.createNewFile
         // load file contents
         printToFile(fo) { p =>
-          scala.io.Source.fromFile(x).getLines.toList.foreach { line =>
+          scala.io.Source.fromFile(x).getLines.toList.map { line =>
 
-            (line.contains("<!--TMPL_VAR NAME="), line.contains("<!--TMPL_IF NAME=")) match {
+            (line.contains("TMPL_VAR NAME"),
+              (line.contains("TMPL_IF") || line.contains("TMPL_UNLESS"))
+            ) match {
               case (true, true) =>
-                val trimTmplVar = """<!--TMPL_VAR NAME=\"(.*?)\".*-->""".r.replaceAllIn(line, m => "@"+m.group(1))
-                val trimTmplIf  = """<!--TMPL_IF NAME=\"(.*?)\".*-->(.*?)<!--/TMPL_IF-->""".r.replaceAllIn(trimTmplVar, m => "@if ("+m.group(1)+".nonEmpty) { " + m.group(2) + " } ")
-                p.println(trimTmplIf)
+                tmplVarAndIfElseToScala(line)
               case (true, false) =>
-                val trimTmplVar = """<!--TMPL_VAR NAME=\"(.*?)\".*-->""".r.replaceAllIn(line, m => "@"+m.group(1))
-                p.println(trimTmplVar)
+                tmplVarToScala(line)
               case (false, true) =>
-                val trimTmplIf  = """<!--TMPL_IF NAME=\"(.*?)\".*-->(.*?)<!--/TMPL_IF-->""".r.replaceAllIn(line, m => "@if ("+m.group(1)+".nonEmpty) { " + m.group(2) + " } ")
-                p.println(trimTmplIf)
+                tmplIfElseToScala(line)
               case (false, false) =>
-                p.println(line)
+                line
             }
+          }.foreach { line =>
+            p.println(line)
           }
         }
       }
