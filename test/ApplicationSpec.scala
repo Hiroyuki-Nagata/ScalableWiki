41712820c6e02ee8d2410acd04616672e9015c0d
hiroyuki nagata <newserver002@gmail.com>
2015-11-26 23:22:28 +0900

    Add test sample

---
+++ test/ApplicationSpec.scala
@@ -0,0 +1,29 @@
+package test
+
+import org.specs2.mutable.Specification
+import play.api.test.{ FakeRequest, FakeApplication }
+import play.api.test.Helpers._
+
+class ApplicationSpec extends Specification {
+
+  "Application" should {
+
+    "Root must return HTML" in {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/"))
+        status(result.get) mustEqual OK
+        //val text = contentAsString(result.get)
+        //println(text)
+        //text must not empty
+      }
+    }
+
+    "/swiki also must return HTML" in {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/swiki"))
+        println(result)
+        result must not beNone
+      }
+    }
+  }
+}
